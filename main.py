2f3768f4f7340a7d0021f6edd1a24d7a227d566a
Lemon_king <oliverdclements@gmail.com>
2022-09-10 20:30:26 +1200

    Comment changes

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import re
+#  Doest really do anything other than a test. Remove???
 from Code import *
 
-def main(spread_sheet_url):
+def cost_dict_make(spread_sheet_url, FN_index):
     """Main function that directs all others"""
     id_key = sheet_id(spread_sheet_url)
     work_sheet, wks = return_sheet(id_key)
-    cost_dict = power_dict_make(work_sheet)
+    cost_dict = power_dict_make(work_sheet, FN_index)
     return cost_dict
 
-main(URL)
